326483eda83c529347910f1a2b51bcf1211a9072
baedonggeun <hispanie3@gmail.com>
2023-12-26 12:00:25 +0900

    카드에 랜덤 이미지 삽입
    카드 종류 만큼 배열 생성
    배열에 요소를 0~7 2개씩 생성
    System.Linq를 참조하여 OrderBy 함수를 이용해 순서 섞기
    프로그램 코드 사용을 위해 Images 에서 image 들을 Resources 파일로 이동
    만들어진 card front에 이미지 붙여넣기

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class gameManager : MonoBehaviour
 {
@@ -12,12 +13,25 @@ public class gameManager : MonoBehaviour
 
     float time;
 
+    int card_count = 16; //게임에 사용되는 카드 전체 개수
+    int card_type = 16; //카드 종류
+
     // Start is called before the first frame update
     void Start()
     {
         Time.timeScale = 1.0f;
 
-        for(int i = 0; i<16; i++)
+        int[] image_N = new int[card_type];
+
+        for (int j = 0; j < card_count; j++)
+        {
+            image_N[j] = j % (card_count / 2);
+        }
+
+        image_N = image_N.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToArray();
+
+
+        for (int i = 0; i< card_count; i++)
         {
             GameObject newCard = Instantiate(card);
             newCard.transform.parent = GameObject.Find("cards").transform;
@@ -25,6 +39,9 @@ public class gameManager : MonoBehaviour
             float x = (i % 4) * 1.4f - 2.1f;
             float y = -(i / 4) * 1.9f + 2.5f;
             newCard.transform.position = new Vector3(x, y, 0);
+
+            string cardName = "card" + image_N[i].ToString();
+            newCard.transform.Find("front").GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(cardName);
         }
     }
 

4285a0895b263bc0608741ae3e8de360eab2aa99
baedonggeun <hispanie3@gmail.com>
2023-12-27 14:53:56 +0900

    Scene별로 특정 배경음 재생
    audioManager singleton화 / DontDestroyOnLoad 함수 사용하여 Scene 전환되어도 Object 파괴되지 않도록 함
    
    bgmList 를 배열로 만들어 손쉽게 배경음 추가 구현
    
    OnSceneLoaded 함수로 Scene과 같은 이름의 bgm 재생
    
    BgSoundPlay 함수를 생성 / bgm의 loop, volume 등 세부 사항 설정 및 bgm 플레이

--- Assets/Scripts/audioManager.cs
+++ Assets/Scripts/audioManager.cs
@@ -1,19 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class audioManager : MonoBehaviour
 {
-    public AudioClip bgmusic;
+    //sound 를 한 곳에서 관리할 수 있도록 singleton 화
+    private static audioManager _instance;
+
+    private void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = this;
+
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+        else if(_instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        //Scene이 전환되어도 object가 없어지지 않도록 함
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public AudioClip[] bgmList;
     public AudioSource audioSource;
+
+    //Scene 이 로딩됐을 때 해당 Scene 이름과 같은 이름의 bgm 재생
+    private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        for(int i = 0; i < bgmList.Length; i++)
+        {
+            if(arg0.name == bgmList[i].name)
+            {
+                BgSoundPlay(bgmList[i]);
+            }
+        }
+    }
+    
+    //배경음 플레이 함수
+    public void BgSoundPlay(AudioClip clip)
+    {
+        audioSource.clip = clip;
+        audioSource.loop = true;
+        audioSource.volume = 0.1f;
+        audioSource.Play();
+    }
+
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        audioSource.clip = bgmusic;
-        audioSource.Play();
+
     }
 
     // Update is called once per frame

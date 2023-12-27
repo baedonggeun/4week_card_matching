7f4cd35499cddf200505681ec1ff33e0d84482e2
baedonggeun <hispanie3@gmail.com>
2023-12-27 16:02:52 +0900

    일부 주석 작성 및 시간, 카드 y값 변경

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -39,28 +39,33 @@ public class gameManager : MonoBehaviour
 
         int[] card_N = new int[card_count];
 
+        //전체 카드 수만큼 배열 생성 및 값 입력
         for (int i = 0; i < card_count; i++)
         {
             card_N[i] = i;
         }
 
+        //랜덤 섞기
         card_N = card_N.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToArray();
 
+        //카드 짝을 맞추기 위해 카드 수의 절반으로 나누고 그 값을 입력
         for(int i = 0; i < card_count; i++)
         {
             card_N[i] = card_N[i % (card_count / 2)];
         }
 
+        //랜덤 섞기 << 2번에 나눠서 하는 이유는 짝이 안 맞을 수 있기 때문
         card_N = card_N.OrderBy(item => Random.Range(-1.0f, 1.0f)).ToArray();
 
 
+        //전체 카드 개수만큼 instance 생성
         for (int i = 0; i< card_count; i++)
         {
             GameObject newCard = Instantiate(card);
             newCard.transform.parent = GameObject.Find("cards").transform;
 
             float x = (i % 4) * 1.4f - 2.1f;
-            float y = -(i / 4) * 1.9f + 2.5f;
+            float y = -(i / 4) * 1.9f + 2.9f;
             newCard.transform.position = new Vector3(x, y, 0);
 
             string cardName = "card" + card_N[i].ToString();
@@ -74,6 +79,7 @@ public class gameManager : MonoBehaviour
         time -= Time.deltaTime;
         timeText.text = time.ToString("N2");
 
+        //15초 이하면 timeText 색상 붉은색으로 변경 + urgent sound 재생
         if(time <= 15.0f)
         {
             timeText.text = "<color=#960707>" + time.ToString("N2") + "</color>";
@@ -81,6 +87,7 @@ public class gameManager : MonoBehaviour
             audioSource.PlayOneShot(urgent);
         }
 
+        //0.00초 이하일 경우 게임 종료
         if(time <= 0.00f)
         {
             Invoke("GameEnd", 0.0f);

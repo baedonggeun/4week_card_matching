9d733b17fec95bef2424b7d18968d2f683e01d2f
baedonggeun <hispanie3@gmail.com>
2023-12-28 16:39:58 +0900

    주석 처리
    추가적인 주석 작성

--- Assets/Scripts/audioManager.cs
+++ Assets/Scripts/audioManager.cs
@@ -5,19 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class audioManager : MonoBehaviour
 {
-    //sound 를 한 곳에서 관리할 수 있도록 singleton 화
     private static audioManager Audio;
 
+    // 게임 시작과 동시에 싱글톤을 구성
     private void Awake()
     {
-        if(Audio == null)
+        // 싱글톤 변수 instance가 비어있는가?
+        if (Audio == null)
         {
+            // Audio가 비어있다면(null) 그곳에 자기 자신을 할당
             Audio = this;
 
+            //sceneLoaded 를 통해 OnSceneLoaded 함수를 Scene Load 시, 실행
             SceneManager.sceneLoaded += OnSceneLoaded;
         }
-        else if(Audio != this)
+        // Audio에 이미 다른 GameManager 오브젝트가 할당되어 있는 경우
+        else if (Audio != this)
         {
+            // 씬에 두개 이상의 GameManager 오브젝트가 존재한다는 의미.
+            // 싱글톤 오브젝트는 하나만 존재해야 하므로 자신의 게임 오브젝트를 파괴
             Destroy(gameObject);
         }
 
@@ -29,6 +35,7 @@ public class audioManager : MonoBehaviour
     public AudioSource audioSource;
 
     //Scene 이 로딩됐을 때 해당 Scene 이름과 같은 이름의 bgm 재생
+    //SceneManager.sceneLoaded += OnSceneLoaded;로 인해 Scene 전환 시, 매 Scene마다 호출됨
     private void OnSceneLoaded(Scene arg0, LoadSceneMode arg1)
     {
         for(int i = 0; i < bgmList.Length; i++)
